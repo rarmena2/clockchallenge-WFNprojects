1136c9c68cd0507924c888a9901513228f032335
rarmena2 <84240274+rarmena2@users.noreply.github.com>
2021-05-15 18:40:40 -0400

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,12 @@
+function showTime() {
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    var period = 'AM';
+    console.log(today);
+    console.log(hours);
+    console.log(today.getTime);
+    console.log('ok');
+}
+showTime();
